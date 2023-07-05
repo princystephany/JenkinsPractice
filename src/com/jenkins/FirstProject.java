be7fbeae9a51c5091acfab675f8ede918e75670e
princystephany <stephanyben6@gmail.com>
2023-07-05 13:30:40 -0700

    checking web hooks

--- src/com/jenkins/FirstProject.java
+++ src/com/jenkins/FirstProject.java
@@ -5,6 +5,10 @@ public class FirstProject {
 	public static void main(String[] args) {
 		
 		System.out.println("Princy");
+		System.out.println("Princy");
+
+		System.out.println("Princy");
+
 	}
 
 }
